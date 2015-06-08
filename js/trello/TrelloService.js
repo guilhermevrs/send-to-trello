a710b7d9178c992627f5b161bb7237dd2a4f44f1
guilhermevrs <guilhermevrs@gmail.com>
2015-06-09 00:08:31 +0200

    Testing the auth token interaction

--- js/trello/TrelloService.js
+++ js/trello/TrelloService.js
@@ -4,7 +4,27 @@
 
 angular.module('app.trello', [])
 .factory('TrelloService', function TrelloService(){
-    var TrelloService = {};
+    var TrelloService = {
+        //localStorage authtoken position
+        LOCAL_STORAGE_AUTH_TOKEN: 'send-to-trello-auth-token',
+
+        //Gets the local auth token
+        getLocalToken: function(){
+            return localStorage[this.LOCAL_STORAGE_AUTH_TOKEN];
+        },
+
+        //Sets the local auth token
+        //@param {string} token - Token to be saved
+        setLocalToken: function(token){
+            localStorage.setItem(this.LOCAL_STORAGE_AUTH_TOKEN, token);
+        },
+
+        //Indicates if the user is logged
+        isUserLogged: function(){
+            var localAuthToken = this.getLocalToken();
+            return localAuthToken !== null && localAuthToken !== undefined;
+        }
+    };
 
     return TrelloService;
 });
--- test/trello/TrelloServiceSpec.js
+++ test/trello/TrelloServiceSpec.js
@@ -5,8 +5,10 @@ describe('TrelloService', function(){
     beforeEach(module('app.trello'));
 
     var TrelloService;
+    var randomFloat;
     beforeEach(inject(function (_TrelloService_) {
         TrelloService = _TrelloService_;
+        randomFloat = Math.random();
     }));
 
     //GENERAL
@@ -19,11 +21,24 @@ describe('TrelloService', function(){
     //AUTHENTICATION
     describe('in the authentication', function(){
         beforeEach(function(){
-
+            //localStorage.clear();
+            localStorage.removeItem(TrelloService.LOCAL_STORAGE_AUTH_TOKEN);
         });
 
-        it('should try to connect over trello', function(){
+        it('should get the authtoken from the right localStorage position', function(){
+            var testValue = 'test_value' + randomFloat;
+            localStorage.setItem(TrelloService.LOCAL_STORAGE_AUTH_TOKEN, testValue);
+            expect(TrelloService.getLocalToken()).toEqual(testValue);
+        });
 
+        it('should set the authtoken in the right localStorage position', function(){
+            var testValue = 'test_value' + randomFloat;
+            TrelloService.setLocalToken(testValue);
+            expect(localStorage[TrelloService.LOCAL_STORAGE_AUTH_TOKEN]).toEqual(testValue);
+        });
+
+        it('should tell when the user is not logged in', function(){
+            expect(TrelloService.isUserLogged()).toBe(false);
         });
     });
 });
