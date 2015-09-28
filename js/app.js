003bf8bc5d15bffde6b521a619a5040944e08450
guilhermevrs <guilhermevrs@gmail.com>
2015-09-28 23:44:33 +0200

    Retrieving all the boards

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ var app = angular.module('app', [
 ]);
 
 app.controller('mainController', function($scope, TrelloService){
+    TrelloService.getBoards(false).then(function(boards){
+        console.log(JSON.stringify(boards));
+    });
+
     $scope.trelloInfo = {
         currentBoard: 'hue'
     };
